e005005fe914818d7f723a8333d4df2d848c1950
James <jamesisom310@gmail.com>
2021-08-04 19:45:08 -0400

    Revert "fixed glitch where sign out message wouldn't appear"
    This reverts commit 447bed1d7a6ceaea685a08770e5521895f2c82a8.

--- registrationSystem/main.cpp
+++ registrationSystem/main.cpp
@@ -116,6 +116,7 @@ m.Login(); //goes to the Login() function so the user can login to their account
 if(m.loginCommand != "return"){
 cout << "Thank you for signing in. Since this is a test, you will automatically be signed out\n";
 sleep_until(system_clock::now() + seconds(3)); // Small delay to imitate a system logging someone out automatically
+cout << "Thank you for signing in. Since this is a test, you will automatically be signed out";
 cout << m.inputUsername << " you have been signed out\n"; //give the user the chance to enter the options again
 }
 cout << "Please type one of these options to proceed: register/login/exit: ";
